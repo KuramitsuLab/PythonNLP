01df24c9ed8d5088cda870626235a6222d2c5263
YagihashiMika <yagihashi.w@gmail.com>
2020-03-05 15:07:51 +0900

    kadai2

---
+++ Yagi/mecab.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import MeCab
+import sys
+import re
+import collections
+from collections import Counter
+
+mecab = MeCab.Tagger ('/usr/local/lib/mecab/dic/mecab-ipadic-neologd')
+
+f = open('../data/kusamakura.txt','r')
+data = f.read() 
+f.close()
+
+parse = mecab.parse(data)
+lines = parse.split('\n')
+items = (re.split('[\t,]', line) for line in lines)
+
+
+# 名詞をリストに格納
+words = [item[0]
+         for item in items
+         if (item[0] not in ('EOS', '', 't', 'ー') and
+             item[1] == '名詞' and item[2] == '一般')]
+
+# 頻度順に出力
+counter = Counter(words)
+for word, count in counter.most_common():
+    print('{0} , {1}'.format(word, count))
+    
